658bccbe4b89046da7cea1c0d6df160e463ef4ea
Taylor C. MacDonald <tcmacdonald@gmail.com>
2014-01-02 11:18:04 -0500

    Add commit_day method to ExerciseEntry.

--- lib/fatsecret/exercise_entry.rb
+++ lib/fatsecret/exercise_entry.rb
@@ -1,12 +1,12 @@
 class FatSecret
   module ExerciseEntry
-    
+
     def self.included(base)
       base.extend(ClassMethods)
     end
-    
+
     module ClassMethods
-      
+
       def get_exercise_entries(oauth_token, oauth_secret, options={})
         query = {
           :method => 'exercise_entries.get',
@@ -24,8 +24,17 @@ class FatSecret
         }
         get(query.merge(options))
       end
-      
+
+      def commit_exercise_entries(oauth_token, oauth_secret, options={})
+        query = {
+          :method => 'exercise_entries.commit_day',
+          :oauth_token => oauth_token,
+          :oauth_secret => oauth_secret
+        }
+        get(query.merge(options))
+      end
+
     end
-    
+
   end
 end
